0cfbe2183a4a9e7f0c12bc431211b8bbf38396d2
jordyedjamf <kibalojordy.edjamfeile@ynov.com>
2018-05-09 23:20:35 +0200

    node

--- src/routes/default.js
+++
@@ -1,7 +0,0 @@
-var controller = require('./../controllers/default'); 
-// On injecte le router d'express, nous en avons besoin pour définir les routes 
-module.exports = function(router) { 
-    // On crée une route sur l'url "/",
-    // cette route va appeler la méthode index() du contrôleur     
-    router.get('/', controller.donut); 
-};
